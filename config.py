57f084ff997721e8fb4d8ebcec3a29a8ae974af6
Abhinai Reddy⛔️ <159715958+abhinai2244@users.noreply.github.com>
2025-12-21 16:55:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,13 +6,13 @@
 # Don't Remove Credit 
 # Telegram Channel @RexBots_Official
 import os
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8026406097:AAEnz8Q19PMMkZYihw2YyB_I5iz9WH2dlJw")
-API_ID = int(os.environ.get("API_ID", "22451491"))
-API_HASH = os.environ.get("API_HASH", "28e74942125f7e4968398ea651cd417b")
-ADMINS = [int(id) for id in os.environ.get("ADMINS", "6574393060,5360305806,5756495153").split(",")]
-DB_URI = os.environ.get("DB_URI", "mongodb+srv://knight_rider:GODGURU12345@knight.jm59gu9.mongodb.net/?retryWrites=true&w=majority")
-DB_NAME = os.environ.get("DB_NAME", "SaveRestricted2")
-LOG_CHANNEL = -1002616517280
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+API_ID = int(os.environ.get("API_ID", ""))
+API_HASH = os.environ.get("API_HASH", "")
+ADMINS = [int(id) for id in os.environ.get("ADMINS", "").split(",")]
+DB_URI = os.environ.get("DB_URI", "")
+DB_NAME = os.environ.get("DB_NAME", "REXSaveRestricted")
+LOG_CHANNEL = 
 ERROR_MESSAGE = bool(os.environ.get('ERROR_MESSAGE', True))
 KEEP_ALIVE_URL = os.environ.get("KEEP_ALIVE_URL", "")
 # Rexbots
