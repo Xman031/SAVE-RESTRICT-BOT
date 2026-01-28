c8114e237b5200dd6d784bd65233ddcc81f4f277
Xman031 <150153244+Xman031@users.noreply.github.com>
2026-01-28 16:49:25 +0530

    Update credits and default values in config.py

--- config.py
+++ config.py
@@ -1,29 +1,29 @@
-# Rexbots
+# @mnkmoviess
 # Don't Remove Credit
-# Telegram Channel @RexBots_Official
+# Telegram Channel @mnkmoviess
 
-# Rexbots
+# @mnkmoviess
 # Don't Remove Credit 
-# Telegram Channel @RexBots_Official
+# Telegram Channel @mnkmoviess
 import os
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 API_ID = int(os.environ.get("API_ID", ""))
 API_HASH = os.environ.get("API_HASH", "")
-ADMINS = [int(id) for id in os.environ.get("ADMINS", "").split("6497825070")]
+ADMINS = [int(id) for id in os.environ.get("ADMINS", "6497825070").split(",")]
 DB_URI = os.environ.get("DB_URI", "")
-DB_NAME = os.environ.get("DB_NAME", "REXSaveRestricted")
+DB_NAME = os.environ.get("DB_NAME", "mnkmoviessSaveRestricted")
 LOG_CHANNEL = -1003787639037
 ERROR_MESSAGE = bool(os.environ.get('ERROR_MESSAGE', True))
 KEEP_ALIVE_URL = os.environ.get("KEEP_ALIVE_URL", "")
-# Rexbots
+# @mnkmoviess
 # Don't Remove Credit
-# Telegram Channel @RexBots_Official
+# Telegram Channel @mnkmoviess
 
 
-# Rexbots
+# @mnkmoviess
 # Don't Remove Credit 
-# Telegram Channel @RexBots_Official
+# Telegram Channel @mnkmoviess
 
-# Rexbots
+# @mnkmoviess
 # Don't Remove Credit
-# Telegram Channel @RexBots_Official
+# Telegram Channel @mnkmoviess
